c9e24e6420e7baa5fddc73240821af1be75e7868
rtraba <rtrabacastagneris@gmail.com>
2018-07-10 17:34:15 -0300

    Added functinality for voting restricting votes to just one per ethereum account (address)

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,28 +1,42 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.2;
 
 contract Election {
-	//Model Candidate
-	struct Candidate{
-		uint id;
-		string name;
-		uint voteCount;
-	}
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-	// Store Candidate
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
 
-	// Fetch Candidate
-	mapping(uint=> Candidate) public candidates;
-	// store candidate voteCount
-	uint public candidatesCount;
+    function Election () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
 
-	function Election() {
-		addCandidate("Candidto 1");
-		addCandidate("Candidto 2");
-		//addCandidate("Candidto 3");
-	}
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
 
-	function addCandidate(string _name) private{
-		candidatesCount ++;
-		candidates[candidatesCount]= Candidate(candidatesCount,_name, 0);
-	} 
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
 }
